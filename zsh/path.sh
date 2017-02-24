d2109446c4bac53274b8bb54bc8e5347d754a334
Timo Mämecke <timo@maemecke.com>
2017-02-24 11:38:07 +0100

    Yarn installed via installation script, needs to set path

--- zsh/path.sh
+++ zsh/path.sh
@@ -2,3 +2,4 @@ export PATH=${PATH}:~/bin
 export PATH=${PATH}:~/dotfiles/bin
 export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+export PATH=${PATH}:~/.yarn/bin
