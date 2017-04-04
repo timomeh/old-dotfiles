eb81eb873ad99665ed4bcc72877d16267d5a76bc
Timo Mämecke <timo@maemecke.com>
2017-04-04 12:05:36 +0300

    .dokku_alias

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,3 +1,4 @@
 alias flushdns='sudo dscacheutil -flushcache && sudo killall -HUP mDNSResponder'
 alias btom='atom-beta'
 alias nuc='ATOM_HOME=~/.nuclide_installation btom'
+alias dokku='bash $HOME/.dokku/contrib/dokku_client.sh'
